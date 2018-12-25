3faf79ba374818ab85257267baaefaee7ca4c041
alexbrocklebank <alexbrocklebank@gmail.com>
2018-12-24 22:52:42 -0500

    Map loads with custom style

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// globals
+var map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 40.7413549, lng: -73.9980244},
+        zoom: 13,
+        styles: multiBrandNetwork,
+        mapTypeControl: true
+    });
+}
