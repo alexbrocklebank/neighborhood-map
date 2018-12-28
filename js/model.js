de0a43a251b62674a75d6b009f78e38e6d74c6c1
alexbrocklebank <alexbrocklebank@gmail.com>
2018-12-27 23:58:25 -0500

    New list of locations added

--- js/model.js
+++ js/model.js
@@ -1,38 +1,93 @@
 var locations = [
     {
-        title: 'Park Ave Penthouse', 
-        lat: 40.7713024, 
-        lng: -73.9632393,
+        title: 'Empire State Building', 
+        lat: 40.705608, 
+        lng: -74.016724,
         type: 'Living Quarters'
     },
     {
-        title: 'Chelsea Loft', 
-        lat: 40.7444883, 
-        lng: -73.9949465,
+        title: 'Whitney Museum of American Art', 
+        lat: 43.043230, 
+        lng: -74.863510,
         type: 'Living Quarters'
     },
     {
-        title: 'Union Square Open Floor Plan', 
-        lat: 40.7347062, 
-        lng: -73.9895759,
+        title: 'Solomon R. Guggenheim Museum', 
+        lat: 40.739580, 
+        lng: -74.006680,
         type: 'Living Quarters'
     },
     {
-        title: 'East Village Hip Studio', 
-        lat: 40.7281777, 
-        lng: -73.984377,
+        title: 'Famous Original Ray\'s Pizza', 
+        lat: 40.783460, 
+        lng: -73.948010,
         type: 'Living Quarters'
     },
     {
-        title: 'TriBeCa Artsy Bachelor Pad', 
-        lat: 40.7195264, 
-        lng: -74.0089934,
+        title: 'The Crooked Knife', 
+        lat: 40.783460, 
+        lng: -73.948010,
         type: 'Living Quarters'
     },
     {
-        title: 'Chinatown Homey Space', 
-        lat: 40.7180628, 
-        lng: -73.9961237,
+        title: 'New York City Hall', 
+        lat: 40.712513, 
+        lng: -74.006546,
+        type: 'Living Quarters'
+    },
+    {
+        title: 'Joe\'s Shanghai', 
+        lat: 40.714580, 
+        lng: -73.997770,
         type: 'Living Quarters'
     }
 ];
+
+/*
+Empire State Building
+20 W 34th St
+New York, NY 10001
+40.705608,
+-74.016724
+
+Griffith Park Observatory
+200 East Observatory Road
+Los Angeles, CA
+34.121380,
+-118.300000
+
+Whitney Museum of American Art
+99 Gansevoort St,
+43.043230,
+-74.863510
+
+Solomon R. Guggenheim Museum
+1971 5h Ave,
+New York, NY 10014
+40.739580,
+-74.006680
+
+Famous Original Ray's Pizza
+1827 2nd Ave,
+New York, NY 10128
+40.783460,
+-73.948010
+
+The Crooked Knife
+29 E 30th St,
+New York, NY 10016
+40.783460,
+-73.948010
+
+New York City Hall
+City Hall Park,
+New York, NY 10007
+40.712513,
+-74.006546
+
+Joe's Shanghai
+9 Pell St
+New York, NY 10013
+40.714580,
+-73.997770
+*/
