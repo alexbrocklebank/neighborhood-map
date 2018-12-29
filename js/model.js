1f2bb64e863420bded5b42ef4bf57658962a2f97
alexbrocklebank <alexbrocklebank@gmail.com>
2018-12-28 22:11:54 -0500

    Remove outlier and comments

--- js/model.js
+++ js/model.js
@@ -5,11 +5,6 @@ var locations = [
         lng: -74.016724
     },
     {
-        title: 'Whitney Museum of American Art', 
-        lat: 43.043230, 
-        lng: -74.863510
-    },
-    {
         title: 'Solomon R. Guggenheim Museum', 
         lat: 40.739580, 
         lng: -74.006680
@@ -35,52 +30,3 @@ var locations = [
         lng: -73.997770
     }
 ];
-
-/*
-Empire State Building
-20 W 34th St
-New York, NY 10001
-40.705608,
--74.016724
-
-Griffith Park Observatory
-200 East Observatory Road
-Los Angeles, CA
-34.121380,
--118.300000
-
-Whitney Museum of American Art
-99 Gansevoort St,
-43.043230,
--74.863510
-
-Solomon R. Guggenheim Museum
-1971 5h Ave,
-New York, NY 10014
-40.739580,
--74.006680
-
-Famous Original Ray's Pizza
-1827 2nd Ave,
-New York, NY 10128
-40.783460,
--73.948010
-
-The Crooked Knife
-29 E 30th St,
-New York, NY 10016
-40.783460,
--73.948010
-
-New York City Hall
-City Hall Park,
-New York, NY 10007
-40.712513,
--74.006546
-
-Joe's Shanghai
-9 Pell St
-New York, NY 10013
-40.714580,
--73.997770
-*/
